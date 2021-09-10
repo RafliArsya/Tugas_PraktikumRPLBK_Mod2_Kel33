ed94b3ab7dab06cbdb071a78a361c88fc77bed52
RafliArsya <47909089+RafliArsya@users.noreply.github.com>
2021-09-10 21:54:51 +0700

    [Puput] Tambah Cardinfo

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 
 import data from './data.json';
 import CardList from './components/CardList';
+import CardInfo from "./components/CardInfo";
 import Quotes from "./components/Quotes";
 
 //import Gambar from './assets/img/nil.gif';
-const quotes = "Never Lose of Sight!";
+const quotes = "Laptop Harga Merakyat Kualitas Sultan";
+const author = "@puput_we";
 
 
 //import reportWebVitals from './reportWebVitals';
@@ -17,6 +19,14 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
     <CardList data={data} />
+
+    <CardInfo
+    title="Promo 9.9 Harga Super MURAH"
+    tglPost="1 September 2021"
+    info="Cermati brosur kami ya dan bandingkan harganya, promo yang kami berikan benar benar termurah di Salatiga, 
+    tidak percaya ? Silakan datang langsung ke Sultan Copm Store di RUKO Pancasila No 8, atau kunjungi Olshop kami di Shoppe : SultanCompStore33 dan dapatkan Extra Cashback hingga 98%"
+    author={author}/>
+
     <Quotes author="Me" quotes={quotes} />
   </React.StrictMode>,
   document.getElementById('root')
