3904c39145843161b7cf0b66f32d311b73dff453
Puputwe <werdiningsihpuput@gmail.com>
2021-09-10 13:39:16 +0700

    Buat Component CardInfo

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -5,7 +5,7 @@ export default function CardList(props) {
     return (
         <div className="bg-white shadow">
             <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Laptop</h2>
+                <h2 className="text-2xl font-extrabold tracking-tight text-indigo-600">Catalog Laptop</h2>
                 <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     
                     {data.map((data) => (
--- src/index.js
+++ src/index.js
@@ -2,22 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 import data from './data.json';
 import CardList from './components/CardList';
+import CardInfo from "./components/CardInfo";
 import Quotes from "./components/Quotes";
 
-//import Gambar from './assets/img/nil.gif';
-const quotes = "Never Lose of Sight!";
+const quotes = "Laptop Harga Merakyat Kualitas Sultan";
+const author = "@puput_we";
 
-
-//import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <CardList data={data} />
-    <Quotes author="Me" quotes={quotes} />
+    <CardInfo
+    title="Promo 9.9 Harga Super MURAH"
+    tglPost="1 September 2021"
+    info="Cermati brosur kami ya dan bandingkan harganya, promo yang kami berikan benar benar termurah di Salatiga, 
+    tidak percaya ? Silakan datang langsung ke Sultan Copm Store di RUKO Pancasila No 8, atau kunjungi Olshop kami di Shoppe : SultanCompStore33 dan dapatkan Extra Cashback hingga 98%"
+    author={author}
+    />
+    <Quotes author="Owner" quotes={quotes} />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -25,4 +32,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+reportWebVitals();
