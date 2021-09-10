c9a6a150c0fbffe5b6847c3ed7ec6918dbc5fab4
RafliArsya <47909089+RafliArsya@users.noreply.github.com>
2021-09-10 21:04:46 +0700

    Reupload stable changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="max-w-7xl mx-auto px-4 sm:px-6">
       <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
         <div className="flex justify-start lg:w-0 lg:flex-1">
-        <h2 className="text-4xl font-extrabold tracking-tight text-indigo-600">Sultan Comp Store</h2>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
             <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
               Sign in
