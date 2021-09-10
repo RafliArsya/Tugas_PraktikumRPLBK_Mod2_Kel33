552b4acec6f22793b70801870f0141ecc874d911
Puputwe <werdiningsihpuput@gmail.com>
2021-09-10 13:48:48 +0700

    Edit Component Quotes

--- src/components/Quotes/index.js
+++ src/components/Quotes/index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Quotes extends Component {
     render() {
         return (
-        <div className="flex flex-col items-center justify-center bg-gray-50 shadow-md" style={{ height: "200px" }}>
+        <div className="flex flex-col items-center justify-center bg-indigo-400 shadow-md" style={{ height: "200px" }}>
             <h3 className="font-bold text-xl">{this.props.quotes}</h3>
             <p>-{this.props.author}</p>
         </div>
